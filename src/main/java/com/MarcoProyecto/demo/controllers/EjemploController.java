9a50440e22d9b525eb0dea443de2c9d786ab27de
MarcoAlMur <marco.alonso@estudiante.epsum.school>
2026-06-16 11:27:46 +0000

    Creado Map para enviar info a la pag web Modelo Vista Controlador

--- src/main/java/com/MarcoProyecto/demo/controllers/EjemploController.java
+++ src/main/java/com/MarcoProyecto/demo/controllers/EjemploController.java
@@ -1,6 +1,9 @@
 package com.MarcoProyecto.demo.controllers;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -8,7 +11,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class EjemploController {
 
     @GetMapping("/detalles_info")
-    public String info() {
+    public String info(Map<String, Object> model) {
+        model.put("Titulo", "Servidor en línea!");
+        model.put("Servidor", "MarcoAlon");
+        model.put("IP", "192.168.2.100");
 
         return "detalles_info";
     }
